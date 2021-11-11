33764dcb91cf1dbf566c646cf54025db9f966886
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-11-11 02:02:29 +0100

    basic page looking

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class CreateExercises extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Create Exercises list </p>
+      </div>
+    );
+  }
+}
