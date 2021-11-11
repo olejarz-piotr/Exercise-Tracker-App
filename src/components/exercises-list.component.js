38a99fa551925bd29b7e9b66da4a991ca1844ef8
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-11-11 18:30:02 +0100

    creating user and exercise

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-export default class ExercisesList extends Component {
-  render() {
-    return (
-      <div>
-        <p>You are on the exercises list </p>
-      </div>
-    );
-  }
-}
+
+const exercisesList = () => {
+  return (
+    <div>
+      <p>You are on the exercises list </p>
+    </div>
+  );
+};
+export default exercisesList;
